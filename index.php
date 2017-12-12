266c32ce7536216f7e45a15ae833ab892ce8ccc4
fedorcakr <rastislav.fedorcak@gmail.com>
2017-12-12 15:00:04 +0100

    upload zakladu

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+	<meta charset="UTF-8" />
+	<title>web site</title>
+	<link rel="stylesheet" type="text/css" href="" media="all" />
+</head>
+<body>
+
+<!-- hlavicka -->
+<header>
+</header>
+	<!-- navigacia a telo -->
+<body>
+</body>
+</html>
